cd90977bed7ae12904985cfadeec82e7882df986
kimwwk <kimwong.wwk@gmail.com>
2025-12-18 20:29:04 -0500

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ourpot.app',
+  appName: 'our-pot',
+  webDir: 'public'
+};
+
+export default config;
